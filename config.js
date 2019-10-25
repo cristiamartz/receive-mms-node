cc5617b4838324f4bdaca6269a31410fcfc4c3fd
cristiamartz <chriix_1105@hotmail.com>
2019-10-25 16:58:56 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,3 @@
-
-//  TWILIO_ACCOUNT_SID='ACfe3b3e808ada63cdf70719edcce47688';
-//  TWILIO_AUTH_TOKEN='3cc88d9f1db2595a964d8f1fda400c5a';
-//  TWILIO_NUMBER=+14155238886
-
-
 const cfg = {};
 
 // HTTP Port to run our web application
@@ -15,14 +9,12 @@ cfg.port = process.env.PORT || 3000;
 // A good practice is to store these string values as system environment
 // constiables, and load them from there as we are doing below. Alternately,
 // you could hard code these values here as strings.
-
-
-cfg.twilioAccountSid = process.env.TWILIO_ACCOUNT_SID='ACfe3b3e808ada63cdf70719edcce47688';
-cfg.twilioAuthToken = process.env.TWILIO_AUTH_TOKEN='3cc88d9f1db2595a964d8f1fda400c5a';
+cfg.twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+cfg.twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 
 // A Twilio number you control - choose one from:
 // Specify in E.164 format, e.g. "+16519998877"
-cfg.twilioPhoneNumber = process.env.TWILIO_NUMBER=+14155238886;
+cfg.twilioPhoneNumber = process.env.TWILIO_NUMBER;
 
 // Export configuration object
 module.exports = cfg;
