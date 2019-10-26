59b0872af971dd765ac62bdd0f4379baadb9a42c
cristiamartz <chriix_1105@hotmail.com>
2019-10-26 13:09:13 -0500

    Update send-mms.js

--- send-mms.js
+++ send-mms.js
@@ -1,12 +1,12 @@
 const config = require('./config');
 const Twilio = require('twilio');
 
-const { twilioAccountSid, twilioAuthToken, twilioPhoneNumber } = config;
+const { twilioAccountSid, twilioAuthToken} = config;
 const client = new Twilio(twilioAccountSid, twilioAuthToken);
 
 client.messages.create({
-  to: +573173310654,
-  from: twilioPhoneNumber,
+  to: 'whatsapp:+573173310654',
+  from: 'whatsapp:+14155238886',
   body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
   mediaUrl: 'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg',
 })
